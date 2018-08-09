66efafad71c4a7af03160e7d8475b121eb546ed4
a949737116 <949737116@qq.com>
2018-08-09 17:12:27 +0800

    基础页面开发DONE

--- admin/router/index.js
+++ admin/router/index.js
@@ -7,4 +7,28 @@ router.get('/', function (req, res, next) {
 router.get('/user', function (req, res, next) {
   res.render('user')
 })
+router.get('/class', function (req, res, next) {
+  res.render('class')
+})
+router.get('/list', function (req, res, next) {
+  res.render('articleList')
+})
+router.get('/addClass', function (req, res, next) {
+  res.render('class_add')
+})
+router.get('/todo', function (req, res, next) {
+  res.render('todoList')
+})
+router.get('/todo2', function (req, res, next) {
+  res.render('todoList2')
+})
+router.get('/tip', function (req, res, next) {
+  res.render('tips')
+})
+router.get('/mc', function (req, res, next) {
+  res.render('modifyClass')
+})
+router.get('/aBlog', function (req, res, next) {
+  res.render('addBlog')
+})
 module.exports = router
